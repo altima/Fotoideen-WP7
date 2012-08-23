edaac00b299bf339b0868eaeab79caf0a4d7b9d7
Altima <mail@enricomeinel.de>
2012-08-23 21:52:38 +0200

    updated to version 1.2

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("wp7dev.de")]
 [assembly: AssemblyProduct("Fotoideen")]
-[assembly: AssemblyCopyright("Copyright © wp7dev.de 2011")]
+[assembly: AssemblyCopyright("Copyright © wp7dev.de 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,8 +32,8 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
 [assembly: NeutralResourcesLanguageAttribute("de")]
 
 [assembly: Obfuscation(Feature = "encrypt symbol names with password altima", Exclude = false)]
